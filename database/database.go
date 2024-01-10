e240564ab43d0fb3b11be9277c9cf83b347243b3
Patrik Chytil <patrik730703@gmail.com>
2024-01-10 21:21:15 +0100

    initial commit

---
+++ database/database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+    "log"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
+    "os"
+)
+
+var Database *gorm.DB
+
+func Connect() {
+    var err error
+    host := os.Getenv("DB_HOST")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbname := os.Getenv("DB_NAME")
+    port := os.Getenv("DB_PORT")
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbname)
+    log.Printf("DSN is: %s\n", dsn)
+    Database, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+    if err != nil {
+        panic(err)
+    } else {
+        log.Println("Successfully connected to the database")
+    }
+}
